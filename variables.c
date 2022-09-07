6dd4c903b45f911ec7bb7d0d41cd0c2f60c762db
unknown <yassin2000ziko@gmail.com>
2022-09-07 09:45:31 +0100

    first commit

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int age; 		//declaration d'une variable de type int
+	age = 22;		// Initialisation de la variable age
+	printf("Yassin is %d years old \n", age); 
+	
+	age = 21;      // On a changer la valeur de la variable a
+	
+	printf("Wissal is %d years old \n", age); 
+	
+	return 0;
+		
+}
