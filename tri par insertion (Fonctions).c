56540ee8b9bdcb692a4d0a72b197f0fa32f3b388
nissay <yassin2000ziko@gmail.com>
2022-09-14 17:09:26 +0100

    Fix some issues

--- tri par insertion (Fonctions).c
+++ tri par insertion (Fonctions).c
@@ -10,7 +10,7 @@ void insertionSort(int arr[], int size){
 	for(i =0; i < size ; i++){
 		current = arr[i];
 		j = i;
-		while(j >=  1 && arr[j -1] > current){
+		while(j > 0 && arr[j -1] > current){
 			arr[j] = arr[j-1];
 			j--;	
 		}
